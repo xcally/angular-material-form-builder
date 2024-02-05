a8f5622aca70110bac8ad3282dd2ef61c6edc6b7
Massimiliano Bungaro <massimiliano.bungaro@xcally.com>
2024-02-05 15:33:37 +0100

    Add date limit for date/datetime-local fields

---
+++ dist/angular-material-form-builder.9af813b6bce0da478426.hot-update.js
@@ -0,0 +1,27 @@
+"use strict";
+self["webpackHotUpdateangular_material_form_builder"]("angular-material-form-builder",{
+
+/***/ "./src/lib/directives/date-item/date-view.tpl.html":
+/*!*********************************************************!*\
+  !*** ./src/lib/directives/date-item/date-view.tpl.html ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("<md-input-container class=\"md-block\">\n  <input\n    ng-attr-max=\"{{DateView.formItem.config.type === 'date' ?\n  '9999-12-31' : (DateView.formItem.config.type === 'datetime-local' ? '9999-12-31T23:59' : undefined)}}\"\n    ng-model=\"DateView.formItem.value\"\n    type=\"{{DateView.formItem.config.type}}\"\n    ng-required=\"DateView.formItem.config.required\"\n  />\n  <div ng-messages=\"DateView.form.$error\">\n    <div ng-message=\"required\">This field is required</div>\n  </div>\n</md-input-container>\n");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("741f9e6724e8bf960d09")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=angular-material-form-builder.9af813b6bce0da478426.hot-update.js.map
